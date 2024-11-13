3f18ff0ea2bc3a44186dc71b6aae4191e55ef1df
FYLSen <cedaren@gmail.com>
2024-11-13 09:52:16 +0800

    fix: update web adsense

--- src/components/head/WebAdSense.tsx
+++ src/components/head/WebAdSense.tsx
@@ -3,17 +3,16 @@ import { adsense } from '@/config.json'
 export function WebAdSense() {
   if (import.meta.env.DEV || !adsense.enable) return null
 
-  return (
-    <>
-      {adsense.google.client && <GoogleAdSense {...adsense.google} />}
-    </>
-  )
+  return <>{adsense.google.client && <GoogleAdSense {...adsense.google} />}</>
 }
 
 function GoogleAdSense({ client }: { client: string }) {
   return (
     <>
-      <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${client}`} />
+      <script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${client}`}
+      />
     </>
   )
 }
